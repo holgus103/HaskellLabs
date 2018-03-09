01f4065977027be585e8caf775d767a11f216814
holgus103 <holgus103@gmail.com>
2018-03-09 12:03:20 +0100

    After lab 3

--- lab3/lab3.hs
+++ lab3/lab3.hs
@@ -1,4 +1,6 @@
 module Lab3 where
+
+import Data.List (foldl')
 -- zad 1
 -- recurrency version
 contains :: Eq a => [a] -> a -> Bool 
@@ -52,3 +54,55 @@ quicksort (x:xs) =
         greater = filter (\e -> e > x) xs
 
 -- zad 5 
+isDescending [] = (True, 0)
+isDescending (x:xs) = 
+    foldl' folder (True, x) xs
+    where
+        folder (isSorted, last) e =
+            if isSorted && last >= e then (True, e)
+            else (False, e)
+
+-- zad 6 
+sumNums l1 l2 =     
+    acc
+    where
+        len1 = length l1
+        len2 = length l2
+        alligned1 = if len1 < len2 then (take (len2 - len1) $ repeat 0) ++ l1 else l1
+        alligned2 = if len2 < len1 then (take (len1 - len2) $ repeat 0) ++ l2 else l2
+        r1 = reverse alligned1
+        r2 = reverse alligned2
+        zipped = zip r1 r2
+        folder (val, acc) (e1,e2) =
+            let sum = e1 + e2 + acc 
+            in
+                if sum >= 10 then ((sum - 10):val, 1)
+                else (sum:val, 0)
+        (acc,_) = foldl folder ([], 0) zipped
+
+-- zad 7
+eratostenes n =
+    etatostenes' [2..n] [2..n]
+    where
+        etatostenes' [] r = r
+        etatostenes' (x:xs) result =
+            x:(etatostenes'  av rs)
+            where 
+                av = filter (\e -> (mod e x) /= 0) xs
+                rs = filter (\e -> (mod e x) /= 0) result
+
+-- zad 8 
+data Numb = Zero | Succ Numb
+
+instance (Show a) => Show (Numb a) where
+    show Zero  = "0"
+    show Succ a = "0" ++ show a
+
+
+-- laborki punktowane:
+-- typ danych
+-- type klasy
+-- operacje
+-- unikanie wycieków pamieci
+-- rekurencja ogonkowa
+-- operacje na listach
