12038603ae8433d50cb66d16a607d6aba669c6dd
holgus103 <kubasuchan@hotmail.com>
2018-03-14 12:20:51 +0100

    Exercises 1 to 10

--- 99problems/solutions.hs
+++ 99problems/solutions.hs
@@ -51,3 +51,68 @@ rev [x] n = x:n
 
 rev (x:xs) acc = 
     rev xs (x:acc)
+
+-- problem 6
+-- palindromes
+
+isPalindrome i = 
+    helper i $ reverse i
+    where
+        helper [] [] = True
+        helper _ [] = False
+        helper [] _ = False
+        helper (x:xs) (y:ys)
+            | x == y = helper xs ys
+            | otherwise =  False
+
+-- problem 7
+-- flatmap non-recursive
+
+flatMap x =
+    [i | j <- x, i <- j]
+
+-- flatmap recursive
+data NestedList a = Elem a | List [NestedList a]
+
+flatMapR (Elem e) =
+     [e]
+
+flatMapR (List l) = 
+    concatMap flatMapR l
+
+-- sample call: flatMapR (List [Elem 1, List [Elem 1, List [Elem 1]]])
+
+-- problem 8
+-- compress
+
+compress :: [Char] -> [Char]
+compress l =
+    fst $ foldr folder ([], '-') l
+    where
+        folder e (compressed, last) = 
+            if e == last then  (compressed, last)
+            else (e:compressed, e) 
+
+
+-- problem 9
+-- sublist packing
+
+pack l = 
+    current:res
+    where 
+        folder e (sum, last, current) 
+            | e == last = (sum, last, e:current)
+            | otherwise = (current:sum, e, [e])
+        (res, _, current) =  foldr folder ([], '-', []) l
+
+
+-- problem 10 
+-- kawaii compression
+
+encode l = 
+    (count, last):res
+    where 
+        folder e (acc, last, count)
+            | e == last = (acc, last, count + 1)
+            | otherwise = ((count, last):acc, e, 1)
+        (res, last, count) = foldr folder ([], '-', 0) l
